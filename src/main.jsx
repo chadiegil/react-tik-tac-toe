b1e8705939799c0663095805767a7fb558d3e9a6
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-04-24 22:55:19 +0800

    init

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Square from "./Square.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Square />
+  </React.StrictMode>
+);
